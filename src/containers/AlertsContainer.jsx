d3652955b58c5d3b45d1b6f97435632ec6b8693e
Ian Flynn <ianflynnwork@gmail.com>
2023-04-17 19:03:23 -0400

    Alerts container layed out. Alert component added

--- src/containers/AlertsContainer.jsx
+++ src/containers/AlertsContainer.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import Alert from './components/Alert';
 
-const AlertsContainer = () => {
-  return <div>Alerts</div>;
+const AlertsContainer = ({ alerts }) => {
+  const alertsArr = alerts.map((alert, index) => {
+    return <Alert info={alert} key={index} />;
+  });
+  return (
+    <section id='alerts-container'>
+      <h2>Alerts: {alerts.length}</h2>
+      <ul>{alertsArr}</ul>
+    </section>
+  );
 };
 
 export default AlertsContainer;
