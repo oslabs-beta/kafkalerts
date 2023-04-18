6a814a178c387ac94678db1794a79fed459e11f5
Ian Flynn <ianflynnwork@gmail.com>
2023-04-17 20:37:57 -0400

    navbar starter

--- src/containers/NavBar.jsx
+++ src/containers/NavBar.jsx
@@ -1,10 +1,58 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { useLink } from 'react-aria';
 
-const Navbar = () => {
+const Linwk = (props) => {
+  let ref = useRef(null);
+  let { linkProps } = useLink(props, ref);
+
+  return (
+    <a
+      {...linkProps}
+      ref={ref}
+      href={props.href}
+      target={props.target}
+      style={{ color: 'var(--blue)' }}
+    >
+      {props.children}
+    </a>
+  );
+};
+
+const Link = (props) => {
+  let ref = useRef(null);
+  let { linkProps, isPressed } = useLink(
+    { ...props, elementType: 'span' },
+    ref
+  );
+
+  return (
+    <span
+      {...linkProps}
+      ref={ref}
+      style={{
+        color: isPressed
+          ? 'var(--blue)'
+          : 'var(--spectrum-global-color-blue-700)',
+        textDecoration: 'underline',
+        cursor: 'pointer',
+      }}
+    >
+      {props.children}
+    </span>
+  );
+};
+
+<Link onPress={() => alert('Pressed link')}>Adobe</Link>;
+
+const Navbar = ({ username }) => {
   return (
     <div id='navbar-container'>
-      <h1>KAFKALERTS NAVBAR</h1>
-      <button>Log out</button>
+      <button>Home</button>
+      <h1>kafkAlerts</h1>
+      <div>
+        <p>{username}</p>
+        <button>Log out</button>
+      </div>
     </div>
   );
 };
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -4,11 +4,12 @@ import Feed from '../containers/Feed';
 import './styles.scss';
 
 const Dashboard = () => {
+  const [username, setUsername] = useState('Ian');
   const [alerts, setAlerts] = useState([]);
   const [brokers, setBrokers] = useState([]);
   return (
-    <div id='dashboard-page' class='pages'>
-      <Navbar />
+    <div id='dashboard-page' className='pages'>
+      <Navbar username={username} />
       <Feed alerts={alerts} brokers={brokers} />
     </div>
   );
