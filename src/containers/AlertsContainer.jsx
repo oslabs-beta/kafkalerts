b8a4d0f0d4b7ebb9b1bad051a1ca5207c44087f0
hazelbolivar <107953447+hazelbolivar@users.noreply.github.com>
2023-04-20 10:11:34 -0400

    deletes unnecessary pseudocode

--- src/containers/AlertsContainer.jsx
+++ src/containers/AlertsContainer.jsx
@@ -13,12 +13,3 @@ const AlertsContainer = ({ brokers }) => {
 };
 
 export default AlertsContainer;
-// be passinate engineer that loves to build/ that's it
-//ask them about their job, so they don't feel like they are wasting
-//where does project strategy originate, etc
-
-//so tell me about yourself
-//dont make up on the spot,
-//dont care about personal life, they want, what's your tech story
-//always finish your narrative with a question?
-//keep a story diary
