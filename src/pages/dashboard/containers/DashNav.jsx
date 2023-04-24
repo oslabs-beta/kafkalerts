235da7e04f977ebb86b0c8a6a3a646655e6d8021
Ian Flynn <ianflynnwork@gmail.com>
2023-04-24 19:46:00 -0400

    user nav bar adjustments, user menu popup onclick

--- src/pages/dashboard/containers/DashNav.jsx
+++ src/pages/dashboard/containers/DashNav.jsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AlertsContainer from './AlertsContainer';
 import UserMenu from '../components/UserMenu';
+import logo from '../../../assets/logo.png';
 
-const DashNav = ({ alertingBrokers, username }) => {
+const DashNav = ({ alertingBrokers, username, connectionString }) => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const toggleMenu = () => setMenuOpen(!menuOpen);
   return (
     <nav id='dash-nav'>
       <AlertsContainer alertingBrokers={alertingBrokers} />
-      <button>LOGO</button>
+      <button
+        id='user-menu-button'
+        onClick={toggleMenu}
+        style={{ backgroundColor: menuOpen ? 'grey' : 'antiquewhite' }}
+      >
+        <img src={logo} alt='KA' />
+      </button>
+      <UserMenu
+        username={username}
+        menuOpen={menuOpen}
+        connectionString={connectionString}
+      />
     </nav>
   );
 };
