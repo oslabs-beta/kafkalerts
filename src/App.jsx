c0e98164dfebc51a9785939a27d8df42b8133487
Hazel Bolivar <hazelbolivar@Hazels-MacBook-Pro.local>
2023-04-27 16:51:15 -0400

    KAF-69 integrates jest-axe and reconfigures App

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import Homepage from './pages/homepage/Homepage.jsx';
 import Dashboard from './pages/dashboard/Dashboard.jsx';
 
 const App = () => {
   return (
-    <Routes>
-      <Route exact path='/' element={<Homepage />} />
-      <Route path='/dashboard' element={<Dashboard />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path='/' element={<Homepage />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
