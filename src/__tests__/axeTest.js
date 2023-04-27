507fda162810539fa986fc0f15ee3cde4e2e5fdf
Hazel Bolivar <hazelbolivar@Hazels-MacBook-Pro.local>
2023-04-27 17:10:00 -0400

    KAF-69 cleans up axe test file

--- src/__tests__/axeTest.js
+++ src/__tests__/axeTest.js
@@ -11,16 +11,3 @@ it('should demonstrate this matcher`s usage with react testing library', async (
   
   expect(results).toHaveNoViolations()
 })
-
-// const React = require('react')
-// const { render } =  require('react-dom')
-// const App = require('../App')
-
-// const { axe, toHaveNoViolations } = require('jest-axe')
-// expect.extend(toHaveNoViolations)
-
-// it('should demonstrate this matcher`s usage with react', async () => {
-//   render(<App/>, document.body)
-//   const results = await axe(document.body)
-//   expect(results).toHaveNoViolations()
-// })
