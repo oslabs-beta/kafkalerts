c5cd63114f5ff3bfa56c82902cfeb52b0e3c3a6a
Jeb Stone <jeb.b.stone@gmail.com>
2023-04-27 15:25:55 -0700

    Basic Chart Functionality

--- src/server/controllers/apiController.js
+++ src/server/controllers/apiController.js
@@ -43,12 +43,19 @@ apiController.getActiveBrokers = (req, res, next) => {
 }
 apiController.getURP = (req, res, next) => {
   console.log('Getting URP')
-  fetch('http://localhost:9090/api/v1/query?query=kafka_server_replicamanager_underreplicatedpartitions')
+  const now = new Date();
+  const endTimestamp = Math.floor(now.getTime() / 1000);
+  const sevenDaysAgo = new Date(now.getTime() - 7 * 24 * 60 * 60 * 1000);
+  const startTimestamp = Math.floor(sevenDaysAgo.getTime() / 1000);
+  const step = 86400/24;
+  const queryURL = `http://localhost:9090/api/v1/query_range?query=kafka_server_replicamanager_underreplicatedpartitions&start=${startTimestamp}&end=${endTimestamp}&step=${step}s`;
+  const oldQuery = 'http://localhost:9090/api/v1/query?query=kafka_server_replicamanager_underreplicatedpartitions'
+  fetch(queryURL)
   .then(response => response.json())
   .then(data => {
     //res.locals.metrics = {};
     res.locals.metrics.urp = data.data.result;
-    console.log('we have URP: ', data.status)
+    console.log('we have URP: ', data.data.result.values)
     return next();
   })
   .catch (err => {
@@ -63,10 +70,10 @@ apiController.getURP = (req, res, next) => {
 apiController.getBytesIn = (req, res, next) => {
   console.log('getting Bytes In')
   //fetch('http://localhost:9090/api/v1/query?query=kafka_server_brokertopicmetrics_bytesin_total')
-  fetch('http://localhost:9090/api/v1/query_range?query=kafka_server_brokertopicmetrics_bytesin_total&start=2023-04-24T19:42:30.781Z&end=2023-04-24T19:43:00.781Z&step=15s')
+  fetch('http://localhost:9090/api/v1/query_range?query=kafka_server_brokertopicmetrics_bytesin_total&start=2023-04-27T15:42:30.781Z&end=2023-04-27T15:45:00.781Z&step=15s')
   .then(response => response.json())
   .then(data => {
-    console.log('data has been parsed')
+    console.log('data has been parsed ', data)
     res.locals.metrics.bytesIn = data.data.result;
     console.log('we have Bytes In: ', data.data.result)
     return next();
@@ -82,7 +89,7 @@ apiController.getBytesIn = (req, res, next) => {
 
 apiController.getBytesOut = (req, res, next) => {
   console.log('getting Bytes Out')
-  fetch('http://localhost:9090/api/v1/query?query=kafka_server_brokertopicmetrics_bytesout_total&start=2023-04-24T19:42:30.781Z&end=2023-04-24T19:43:00.781Z&step=15s')
+  fetch('http://localhost:9090/api/v1/query_range?query=kafka_server_brokertopicmetrics_bytesout_total&start=2023-04-27T15:42:30.781Z&end=2023-04-27T15:45:00.781Z&step=15s')
   .then(response => response.json())
   .then(data => {
     res.locals.metrics.bytesOut = data.data.result;
