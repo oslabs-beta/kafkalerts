d730e2e0f15ea18f079533ae4d8b75edae81b4fc
mezzocarattere <annierrosen@gmail.com>
2023-05-03 10:57:27 -0400

    test

--- jest-teardown.js
+++ jest-teardown.js
@@ -1,3 +1,18 @@
 module.exports = (globalConfig) => {
   testServer.close();
 };
+
+// "routes": [
+//   {
+//     "src": "/(.*)",
+//     "dest": "src/api/server.js",
+//     "methods": [
+//       "GET",
+//       "POST",
+//       "PUT",
+//       "PATCH",
+//       "OPTIONS",
+//       "DELETE"
+//     ]
+//   }
+// ],
