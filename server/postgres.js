c73d13a43a2a9573c612c5240a73ab0438fa47a4
Ian Flynn <ianflynnwork@gmail.com>
2023-05-24 20:37:13 -0400

    restore postgres

--- server/postgres.js
+++ server/postgres.js
@@ -1,43 +1,45 @@
-// const { Pool } = require('pg');
-// const dotenv = require('dotenv').config()
-// console.log('pg file ran')
-// const connectionString = process.env.PG_URI;
-// const pool = new Pool({
-//   connectionString,
-// });
+const { Pool } = require('pg');
+const dotenv = require('dotenv').config();
+console.log('pg file ran');
+const connectionString = process.env.PG_URI;
+const pool = new Pool({
+  connectionString,
+});
 
-// pool.connect();
+pool.connect();
 
-// // Define the schema creation query
-// const createSchemaQuery = `CREATE SCHEMA IF NOT EXISTS kafkalerts_schema`;
+// Define the schema creation query
+const createSchemaQuery = `CREATE SCHEMA IF NOT EXISTS kafkalerts_schema`;
 
-// // Run the schema creation query. (none is for a query that expects no response)
-// async function createSchema() {
-//   await pool.query(createSchemaQuery)
-//   .then(() => console.log('Schema created successfully'))
-//   .catch((err) => console.error('Error creating schema:', err));
-// }
+// Run the schema creation query. (none is for a query that expects no response)
+async function createSchema() {
+  await pool
+    .query(createSchemaQuery)
+    .then(() => console.log('Schema created successfully'))
+    .catch((err) => console.error('Error creating schema:', err));
+}
 
-// //TO DO: make table for users
-// const makeUserTable = `CREATE TABLE IF NOT EXISTS users (
-//     username VARCHAR(24) NOT NULL,
-//     password VARCHAR(255) NOT NULL,
-//     broker_ids text[],
-//     PRIMARY KEY (username)
-//  )`;
+//TO DO: make table for users
+const makeUserTable = `CREATE TABLE IF NOT EXISTS users (
+    username VARCHAR(24) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    broker_ids text[],
+    PRIMARY KEY (username)
+ )`;
 
-// async function createUserTable() {
-//   await pool.query(makeUserTable)
-//   .then(() => console.log('Table created successfully'))
-//   .catch((err) => console.error('Error creating users table:', err));
-// }
+async function createUserTable() {
+  await pool
+    .query(makeUserTable)
+    .then(() => console.log('Table created successfully'))
+    .catch((err) => console.error('Error creating users table:', err));
+}
 
-// createSchema();
-// createUserTable();
+createSchema();
+createUserTable();
 
-// module.exports = {
-//   query: (text, params, callback) => {
-//     // console.log('executed query', text);
-//     return pool.query(text, params, callback);
-//   }
-// };
+module.exports = {
+  query: (text, params, callback) => {
+    // console.log('executed query', text);
+    return pool.query(text, params, callback);
+  },
+};
