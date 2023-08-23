cce4c0bbeaea1cd75aa6d9dc7ec5abdd064ca7e6
Ian Flynn <ianflynnwork@gmail.com>
2023-08-23 17:57:09 -0400

    swap to react aria buttons and textfields

--- client/src/pages/loginPage/containers/LoginBox.jsx
+++ client/src/pages/loginPage/containers/LoginBox.jsx
@@ -1,15 +1,35 @@
 import React from 'react';
 import TextField from '../components/TextField';
 import Button from '../components/Button';
+import { useNavigate } from 'react-router-dom';
 const LoginBox = ({ setPassword, setUsername, handleSend }) => {
+  const navigate = useNavigate();
   return (
-    <div id='login-container' className='container'>
-      <TextField id='username' label='Username: ' onChange={setUsername} isRequired />
-      <TextField id='password' label='Password: ' onChange={setPassword} isRequired />
-      <div id='button-box'>
-        <Button id='login' onPress={() => handleSend('login')}>Login</Button>
-        <Button id='signup' onPress={() => handleSend('signup')}>Create Account</Button>
+    <div id='login-container'>
+      <TextField
+        id='username'
+        label='Username: '
+        onChange={setUsername}
+        isRequired
+      />
+      <TextField
+        id='password'
+        label='Password: '
+        onChange={setPassword}
+        isRequired
+      />
+      <div id='account-buttons'>
+        <Button id='login' onPress={() => handleSend('login')}>
+          Login
+        </Button>
+        <Button id='signup' onPress={() => handleSend('signup')}>
+          Sign up
+        </Button>
       </div>
+      <hr />
+      <Button id='demo-button' onClick={() => navigate('/dashboard')}>
+        See a Demo!
+      </Button>
     </div>
   );
 };
