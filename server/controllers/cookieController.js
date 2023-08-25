d21aeed3992712144e8b562be2dd9ba3dca7a2d3
Ian Flynn <ianflynnwork@gmail.com>
2023-08-25 17:05:15 -0400

    removed some packages and moved nodemon to dev depend

--- server/controllers/cookieController.js
+++ server/controllers/cookieController.js
@@ -1,7 +1,6 @@
 const cookieController = {};
 
 cookieController.setCookie = (req, res, next) => {
-  // console.log('we made it to set cookie lol');
   // console.log('verified', res.locals.isVerified);
   // if (res.locals.isVerified) {
   //   console.log('cookieID', res.locals.cookieID);
